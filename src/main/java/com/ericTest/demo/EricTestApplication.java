0bfbce8a1a23621d9e825899959039c2fd8bf717
ericpr99 <ericpr99@gmail.com>
2021-03-03 13:14:45 +0800

    init spring boot

---
+++ src/main/java/com/ericTest/demo/EricTestApplication.java
@@ -0,0 +1,13 @@
+package com.ericTest.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EricTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EricTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ericTest/demo/EricTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ericTest.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EricTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
